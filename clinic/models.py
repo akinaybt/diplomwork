9cbf1c6dcaa6e0fe2648c1ca9772f87a50c1abac
akinaybt <abekmambetova364@gmail.com>
2022-11-08 17:21:54 +0600

    Added new fields to the models

--- clinic/models.py
+++ clinic/models.py
@@ -2,16 +2,18 @@ from django.db import models
 
 
 class Clinic(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, null=False, blank=True)
     foundation_date = models.DateField()
+    hospital_director = models.CharField(max_length=100, null=False, blank=True)
 
     def __str__(self):
         return self.title
 
 
 class Department(models.Model):
-    title = models.CharField(max_length=200)
     clinic = models.ForeignKey('Clinic', on_delete=models.PROTECT)
+    title = models.CharField(max_length=200, null=False, blank=True)
+    head_doctor = models.CharField(max_length=100, null=False, blank=True)
 
     def __str__(self):
         return self.title
