31838431c1545270602a300cb0f67287552fe36a
akinaybt <abekmambetova364@gmail.com>
2022-11-11 19:11:49 +0600

    Created new model UserProfile

--- patient/models.py
+++ patient/models.py
@@ -1,16 +1,27 @@
-# from django.db import models
-# from doctor.models import Doctor, Department
-#
-#
-# class PatientInfo(models.Model):
-#     name = models.CharField(max_length=50, blank=True, null=False)
-#     gender = models.CharField(max_length=200, blank=True, null=False)
-#     marital_Status = models.CharField(max_length=100, blank=True, null=False)
-#     telephone = models.CharField(max_length=30, blank=True, null=False)
-#
-#
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+from doctor.models import Doctor, Department
+import phonenumbers
+
+
+class CustomUser(AbstractUser):
+    gender = models.CharField(max_length=30, blank=True, null=False)
+    marital_status = models.CharField(max_length=30, blank=True, null=False)
+    phone_number = PhoneNumberField(null=True, unique=True, region='KG')
+
+    def __str__(self):
+        """Returns the patient's full name."""
+        return '%s %s' % (self.first_name, self.last_name)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+
+
 # class Appointment(models.Model):
 #     referred_department = models.ForeignKey(Department, on_delete=models.CASCADE)
 #     referred_doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-#     # appointment_date = models.DateTimeField()
-#     # appointment_time =
+    # appointment_date = models.DateTimeField()
+    # appointment_time =
