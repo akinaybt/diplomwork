cc1569a8971ded8cd5661edc53d3276858ce08ef
akinaybt <abekmambetova364@gmail.com>
2022-11-13 03:29:05 +0600

    Changed model CustomUser by adding new fields and deleting others

--- patient/models.py
+++ patient/models.py
@@ -2,22 +2,27 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
-from doctor.models import Doctor, Department
+from doctor.models import DoctorProfile, Department
 import phonenumbers
 
 
 class CustomUser(AbstractUser):
+    is_doctor = models.BooleanField(default=False)
+    is_patient = models.BooleanField(default=False)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile',
+                                primary_key=True)
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     gender = models.CharField(max_length=30, blank=True, null=False)
     marital_status = models.CharField(max_length=30, blank=True, null=False)
     phone_number = PhoneNumberField(null=True, unique=True, region='KG')
 
     def __str__(self):
         """Returns the patient's full name."""
-        return '%s %s' % (self.first_name, self.last_name)
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+        return '%s %s' % (self.name, self.last_name)
 
 
 # class Appointment(models.Model):
