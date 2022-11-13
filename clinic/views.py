83e8509bce2b807a39c73beed1703fe9e66cfb94
akinaybt <abekmambetova364@gmail.com>
2022-11-14 00:35:03 +0600

    Created class-based views for models Clinic and Department

--- clinic/views.py
+++ clinic/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.generics import (UpdateAPIView, CreateAPIView, RetrieveAPIView, DestroyAPIView)
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from .models import Clinic, Department
+from .serializers import ClinicSerializer, DepartmentSerializer
 
-# Create your views here.
+
+class ClinicRetrieveApiView(RetrieveAPIView):
+    serializer_class = ClinicSerializer
+    queryset = Clinic.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class ClinicUpdateApiView(UpdateAPIView):
+    serializer_class = ClinicSerializer
+    queryset = Clinic.objects.all()
+    permission_classes = [IsAdminUser]
+
+
+class ClinicCreateApiView(CreateAPIView):
+    serializer_class = ClinicSerializer
+    queryset = Clinic.objects.all()
+    permission_classes = [IsAdminUser]
+
+
+class ClinicDestroyApiView(DestroyAPIView):
+    serializer_class = ClinicSerializer
+    queryset = Clinic.objects.all()
+    permission_classes = [IsAdminUser]
+
+
+class DepartmentCreateApiView(CreateAPIView):
+    serializer_class = DepartmentSerializer
+    queryset = Department.objects.all()
+    permission_classes = [IsAdminUser]
+
+
+class DepartmentRetrieveApiView(RetrieveAPIView):
+    serializer_class = DepartmentSerializer
+    queryset = Department.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class DepartmentDestroyApiView(DestroyAPIView):
+    serializer_class = DepartmentSerializer
+    queryset = Department.objects.all()
+    permission_classes = [IsAdminUser]
+
