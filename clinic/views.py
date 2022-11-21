ab8c7bdd9a8665d997c7109a8702b3b3448019fd
akinaybt <abekmambetova364@gmail.com>
2022-11-21 23:42:17 +0600

    Changed ApiView on viewsets

--- clinic/views.py
+++ clinic/views.py
@@ -1,49 +1,17 @@
-from rest_framework.decorators import action
 from rest_framework.generics import (UpdateAPIView, CreateAPIView, RetrieveAPIView, DestroyAPIView)
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.response import Response
 from .models import Clinic, Department
 from .serializers import ClinicSerializer, DepartmentSerializer
+from rest_framework import viewsets
 
 
-class ClinicRetrieveApiView(RetrieveAPIView):
-    serializer_class = ClinicSerializer
+class ClinicViewSet(viewsets.ModelViewSet):
     queryset = Clinic.objects.all()
-    permission_classes = [IsAuthenticated]
-
-
-class ClinicUpdateApiView(UpdateAPIView):
     serializer_class = ClinicSerializer
-    queryset = Clinic.objects.all()
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
 
-class ClinicCreateApiView(CreateAPIView):
-    serializer_class = ClinicSerializer
-    queryset = Clinic.objects.all()
-    permission_classes = [IsAdminUser]
-
-
-class ClinicDestroyApiView(DestroyAPIView):
-    serializer_class = ClinicSerializer
-    queryset = Clinic.objects.all()
-    permission_classes = [IsAdminUser]
-
-
-class DepartmentCreateApiView(CreateAPIView):
-    serializer_class = DepartmentSerializer
+class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
-    permission_classes = [IsAdminUser]
-
-
-class DepartmentRetrieveApiView(RetrieveAPIView):
     serializer_class = DepartmentSerializer
-    queryset = Department.objects.all()
-    permission_classes = [IsAuthenticated]
-
-
-class DepartmentDestroyApiView(DestroyAPIView):
-    serializer_class = DepartmentSerializer
-    queryset = Department.objects.all()
-    permission_classes = [IsAdminUser]
-
+    permission_classes = [IsAuthenticated, IsAdminUser]
