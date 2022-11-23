1b511f30da6e0958782e14c4321e02763a315f34
akinaybt <abekmambetova364@gmail.com>
2022-11-23 20:05:09 +0600

    Created viewset for DoctorProfile and APIView for DoctorUser

--- doctor/views.py
+++ doctor/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from .serializers import DoctorUserSerializer, DoctorProfileSerializer
+
+from .models import DoctorProfile, DoctorUser
+
+
+class DoctorProfileViewSet(viewsets.ModelViewSet):
+    queryset = DoctorProfile.objects.all()
+    serializer_class = DoctorProfileSerializer
+
+
+class DoctorUserListView(ListAPIView):
+    queryset = DoctorUser.objects.all()
+    serializer_class = DoctorUserSerializer
