7036973c43a3f81e563c7a6274791c30e93e1844
akinaybt <abekmambetova364@gmail.com>
2022-11-23 20:12:46 +0600

    Created new model Appointment

--- patient/models.py
+++ patient/models.py
@@ -2,31 +2,44 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
-from doctor.models import DoctorProfile, Department
+from doctor.models import DoctorProfile, Department, Schedule
 import phonenumbers
 
 
 class CustomUser(AbstractUser):
-    is_doctor = models.BooleanField(default=False)
-    is_patient = models.BooleanField(default=False)
+    is_patient = models.BooleanField(default=True)
+    phone_number = PhoneNumberField(null=True, unique=True, region='KG')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+GENDER = (
+    ('Male', "Мужчина"),
+    ('Female', 'Женщина')
+)
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile',
                                 primary_key=True)
-    name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    gender = models.CharField(max_length=30, blank=True, null=False)
-    marital_status = models.CharField(max_length=30, blank=True, null=False)
+    gender = models.CharField(max_length=30, choices=GENDER)
     phone_number = PhoneNumberField(null=True, unique=True, region='KG')
 
     def __str__(self):
         """Returns the patient's full name."""
-        return '%s %s' % (self.name, self.last_name)
+        return f"{self.user}'s profile"
 
 
-# class Appointment(models.Model):
-#     referred_department = models.ForeignKey(Department, on_delete=models.CASCADE)
-#     referred_doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    # appointment_date = models.DateTimeField()
-    # appointment_time =
+class Appointment(models.Model):
+    patient = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=False)
+    referred_department = models.ForeignKey(Department, on_delete=models.CASCADE, null=False)
+    referred_doctor = models.ForeignKey(DoctorProfile, on_delete=models.CASCADE, null=False)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE, null=False)
+    appointment_date = models.DateField(null=False)
+    appointment_time = models.TimeField(null=False)
+    confirm_appointment = models.BooleanField(default=False, null=False)
+
+    def __str__(self):
+        return f"Приём у {self.patient}"
+
