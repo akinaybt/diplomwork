ca5b56e97f9dce2ff62e90e7c62b70dfb5bba2e9
akinaybt <abekmambetova364@gmail.com>
2022-11-23 20:19:23 +0600

    Deleted model Allergy

--- medicalcard/models.py
+++ medicalcard/models.py
@@ -2,15 +2,12 @@ from django.db import models
 from patient.models import UserProfile
 
 
-class Allergy(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
-
-
 class Card(models.Model):
     patient = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
-    allergies = models.ForeignKey(Allergy, on_delete=models.CASCADE)
+    allergies = models.CharField(max_length=100, null=True)
     birth_date = models.DateField()
     blood_group = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f"Медицинская карточка {self.patient}"
 
