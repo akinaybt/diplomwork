2326d95bd718b566dfc8e48d4308c7fade6854fb
akinaybt <abekmambetova364@gmail.com>
2022-11-26 17:50:33 +0600

    urls for app clinic

---
+++ clinic/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .models import Clinic, Department
+from . import views
+
+router = DefaultRouter()
+
+router.register('clinic', views.ClinicViewSet)
+router.register('departments', views.DepartmentViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
