be570372349873026965fe26a9d99b54931e7895
akinaybt <abekmambetova364@gmail.com>
2022-12-01 18:55:56 +0600

    admin file

--- patient/admin.py
+++ patient/admin.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser, UserProfile, Appointment
+
+
+admin.site.register(CustomUser)
+admin.site.register(UserProfile)
+admin.site.register(Appointment)
 
