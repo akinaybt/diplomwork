2627d38fc9a9d57e5941d234ab6e22a1fe3cd66e
akinaybt <abekmambetova364@gmail.com>
2022-12-02 18:24:56 +0600

    signal for creating doctor's profile

--- doctor/signal.py
+++ doctor/signal.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import DoctorProfile, DoctorUser
 
@@ -9,5 +9,4 @@ def create_doctor_profile(sender, instance, created, **kwargs):
         print('Doctor signal received!')
         DoctorProfile.objects.create(
             user=instance,
-)
-
+        )
