ccf8dc86cb753bd6657465fa8cf6eee8405a15a4
akinaybt <abekmambetova364@gmail.com>
2022-12-02 18:08:37 +0600

    Changed the way of registering models

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Doctor, Schedule
+from .models import DoctorProfile, Schedule, DoctorUser
 
-admin.site.register(Doctor)
 admin.site.register(Schedule)
+admin.site.register(DoctorUser)
+
+
+@admin.register(DoctorProfile)
+class DoctorProfile(admin.ModelAdmin):
+    list_display = ['id', 'user', 'department', 'experience']
+
