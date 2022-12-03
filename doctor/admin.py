32f115b1184122aeeea029ce3e1f27850cd0cbb1
akinaybt <abekmambetova364@gmail.com>
2022-12-03 19:21:03 +0600

    doctor app admin

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from .models import DoctorProfile, Schedule, DoctorUser
 
-admin.site.register(Schedule)
-admin.site.register(DoctorUser)
+
+@admin.register(DoctorUser)
+class DoctorUser(admin.ModelAdmin):
+    list_display = ['user']
 
 
 @admin.register(DoctorProfile)
 class DoctorProfile(admin.ModelAdmin):
     list_display = ['id', 'user', 'department', 'experience']
 
+
+@admin.register(Schedule)
+class Schedule(admin.ModelAdmin):
+    list_display = ['doctor', 'weekday', 'start_time', 'end_time']
